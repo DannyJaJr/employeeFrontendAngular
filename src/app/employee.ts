16633cf2daf22a75fddebde5ca431ee74a5ff9a9
DannyJaJr <ldanieljajr@gmail.com>
2023-06-29 14:32:57 -0400

    generate service created

---
+++ src/app/employee.ts
@@ -0,0 +1,10 @@
+//the type of the data that will return from the backend
+export interface Employee {
+    id: number;
+    name: string;
+    email: string;
+    jobTitle: string;
+    phone: string;
+    imageUrl: string;
+    employeeCode: string;
+  }
